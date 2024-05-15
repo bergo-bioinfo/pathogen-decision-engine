ac56c79e1eec91a58115221b83c9d2e2a4fe79fc
f.vergallo <f.vergallo@bordeaux.unicancer.fr>
2024-05-15 17:40:43 +0200

    parametrize tag added to test to check each input sample separately

--- test/pytest_decision_engine.py
+++ test/pytest_decision_engine.py
@@ -2,85 +2,42 @@ import pandas as pd
 import pytest
 from pathogen_decision_engine import PathogenDecisionEngine
 
-def load_test_cases():
-    test_cases = {
-            'input_samples': [
-                {"BA": 1},
-                {"BS": 2},
-                {"BS": 3},
-                {"BS": 1, "BP": 1},
-                {"BP": 2},
-                {"BP": 3},
-                {"BA": 1, "PA": 1},
-                {"BS": 2, "PS": 2},
-                {"PS": 1},
-                {"BS": 1},
-                {"PVS": 1, "PM": 1},
-                {"PS": 1, "PM": 1},
-                {"PS": 1, "PM": 2},
-                {"PS": 1, "PP": 2},
-                {"PS": 1, "PP": 3},
-                {"PM": 3},
-                {"PM": 4},
-                {"PM": 2, "PP": 2},
-                {"PM": 2, "PP": 3},
-                {"PM": 1, "PP": 4},
-                {"PM": 1, "PP": 5},
-                {"PA": 1},
-                {"PA": 2},
-                {"PVS": 1, "PS": 1},
-                {"PVS": 1, "PS": 2},
-                {"PVS": 1, "PM": 1, "PP": 1},
-                {"PVS": 1, "PM": 0, "PP": 2},
-                {"PVS": 1, "PM": 2, "PP": 0},
-                {"PVS": 1, "PM": 2, "PP": 2},
-                {"PS": 2},
-                {"PS": 3},
-                {"PS": 1, "PM": 3},
-                {"PS": 1, "PM": 4},
-                {"PS": 1, "PM": 2, "PP": 2},
-                {"PS": 1, "PM": 2, "PP": 3},
-                {"PS": 1, "PM": 1, "PP": 4},
-                {"PS": 1, "PM": 1, "PP": 5},
 
-            ],
-            'expected_results': [
-                'Benign',
-                'Benign',
-                'Benign',
-                'Likely Benign',
-                'Likely Benign',
-                'Likely Benign',
-                'Uncertain significance',
-                'Uncertain significance',
-                'Uncertain significance',
-                'Uncertain significance',
-                'Likely pathogenic',
-                'Likely pathogenic',
-                'Likely pathogenic',
-                'Likely pathogenic',
-                'Likely pathogenic',
-                'Likely pathogenic',
-                'Likely pathogenic',
-                'Likely pathogenic',
-                'Likely pathogenic',
-                'Likely pathogenic',
-                'Likely pathogenic',
-                'Pathogenic',
-                'Pathogenic',
-                'Pathogenic',
-                'Pathogenic',
-                'Pathogenic',
-                'Pathogenic',
-                'Pathogenic',
-                'Pathogenic',
-                'Pathogenic',
-                'Pathogenic',
-                'Pathogenic',
-                'Pathogenic'
-            ]
-        }
-    return test_cases
+common_args = ("input_sample,expected_result", [
+    ({'BA': 1}, 'Benign'),
+    ({'BS': 2}, 'Benign'),
+    ({'BS': 3}, 'Benign'),
+    ({'BS': 1, 'BP': 1}, 'Likely Benign'),
+    ({'BP': 2}, 'Likely Benign'),
+    ({'BP': 3}, 'Likely Benign'),
+    ({'BA': 1, 'PA': 1}, 'Uncertain significance'),
+    ({'BS': 2, 'PS': 2}, 'Uncertain significance'),
+    ({'PS': 1}, 'Uncertain significance'),
+    ({'BS': 1}, 'Uncertain significance'),
+    ({'PVS': 1, 'PM': 1}, 'Likely pathogenic'),
+    ({'PS': 1, 'PM': 1}, 'Likely pathogenic'),
+    ({'PS': 1, 'PM': 2}, 'Likely pathogenic'),
+    ({'PS': 1, 'PP': 2}, 'Likely pathogenic'),
+    ({'PS': 1, 'PP': 3}, 'Likely pathogenic'),
+    ({'PM': 3}, 'Likely pathogenic'),
+    ({'PM': 4}, 'Likely pathogenic'),
+    ({'PM': 2, 'PP': 2}, 'Likely pathogenic'),
+    ({'PM': 2, 'PP': 3}, 'Likely pathogenic'),
+    ({'PM': 1, 'PP': 4}, 'Likely pathogenic'),
+    ({'PM': 1, 'PP': 5}, 'Likely pathogenic'),
+    ({'PA': 1}, 'Pathogenic'),
+    ({'PA': 2}, 'Pathogenic'),
+    ({'PVS': 1, 'PS': 1}, 'Pathogenic'),
+    ({'PVS': 1, 'PS': 2}, 'Pathogenic'),
+    ({'PVS': 1, 'PM': 1, 'PP': 1}, 'Pathogenic'),
+    ({'PVS': 1, 'PM': 0, 'PP': 2}, 'Pathogenic'),
+    ({'PVS': 1, 'PM': 2, 'PP': 0}, 'Pathogenic'),
+    ({'PVS': 1, 'PM': 2, 'PP': 2}, 'Pathogenic'),
+    ({'PS': 2}, 'Pathogenic'),
+    ({'PS': 3}, 'Pathogenic'),
+    ({'PS': 1, 'PM': 3}, 'Pathogenic'),
+    ({'PS': 1, 'PM': 4}, 'Pathogenic')
+])
 
 
 @pytest.fixture(scope='module')
@@ -88,16 +45,13 @@ def test_pathogen_decision_engine():
     path = "data/sovad_rules_table.csv"
     df = pd.read_csv(path)
     df = df.fillna(0)
-
     rule_table = pd.DataFrame(df)
     pde = PathogenDecisionEngine(rule_table)
 
     return pde
 
 
-def test_infer(test_pathogen_decision_engine):
-    test_cases = load_test_cases()
-    input_samples, expected_results = test_cases['input_samples'], test_cases['expected_results']
-    for input_sample, expected_result in zip(input_samples, expected_results):
-        result = test_pathogen_decision_engine.infer(input_sample)
-        assert result[0].lower() == expected_result.lower()
+@pytest.mark.parametrize(*common_args)
+def test_infer(test_pathogen_decision_engine, input_sample, expected_result):
+    result = test_pathogen_decision_engine.infer(input_sample)
+    assert result[0].lower() == expected_result.lower()
